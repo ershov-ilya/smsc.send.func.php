6935bcc33f0d8d04363302ed80e64efbc84483ed
ershov-ilya <ershov.ilya@gmail.com>
2015-08-14 13:05:04 +0300

    smsc.sample.config.php

---
+++ smsc.sample.config.php
@@ -0,0 +1,15 @@
+<?php
+/**
+ * Created by PhpStorm.
+ * Author:   ershov-ilya
+ * GitHub:   https://github.com/ershov-ilya/
+ * About me: http://about.me/ershov.ilya (EN)
+ * Website:  http://ershov.pw/ (RU)
+ * Date: 27.07.2015
+ * Time: 17:45
+ */
+
+$smsc_config=array(
+    'login' => 'your_login',
+    'psw' => 'your_smsc_md5_pass_hash'
+);
